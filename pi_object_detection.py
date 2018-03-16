c6afb50b7b01051fad2d8cf3217b5301e9edfe71
Kevin Dong <dongkl@hotmail.com>
2018-03-15 21:06:27 -0400

    - Add object_detection function using RPi camera (pi_object_detection.py) - Separate deep learning forward pass from main thread (improve display update frame rate)

--- pi_object_detection.py
+++ pi_object_detection.py
@@ -1,14 +1,36 @@
 # USAGE
-# python real_time_object_detection.py --prototxt MobileNetSSD_deploy.prototxt.txt --model MobileNetSSD_deploy.caffemodel
+# python pi_object_detection.py --prototxt MobileNetSSD_deploy.prototxt.txt --model MobileNetSSD_deploy.caffemodel
 
 # import the necessary packages
 from imutils.video import VideoStream
 from imutils.video import FPS
+from multiprocessing import Process
+from multiprocessing import Queue
 import numpy as np
 import argparse
 import imutils
 import time
 import cv2
+
+def classify_frame(net, inputQueue, outputQueue):
+	# keep looping
+	while True:
+		# check to see if there is a frame in our input queue
+		if not inputQueue.empty():
+			# grab the frame from the input queue, resize it, and
+			# construct a blob from it
+			frame = inputQueue.get()
+			frame = cv2.resize(frame, (300, 300))
+			blob = cv2.dnn.blobFromImage(frame, 0.007843,
+				(300, 300), 127.5)
+
+			# set the blob as input to our deep learning object
+			# detector and obtain the detections
+			net.setInput(blob)
+			detections = net.forward()
+
+			# write the detections to the output queue
+			outputQueue.put(detections)
 
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
@@ -32,6 +54,20 @@ COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
 print("[INFO] loading model...")
 net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
 
+# initialize the input queue (frames), output queue (detections),
+# and the list of actual detections returned by the child process
+inputQueue = Queue(maxsize=1)
+outputQueue = Queue(maxsize=1)
+detections = None
+
+# construct a child process *indepedent* from our main process of
+# execution
+print("[INFO] starting process...")
+p = Process(target=classify_frame, args=(net, inputQueue,
+	outputQueue,))
+p.daemon = True
+p.start()
+
 # initialize the video stream, allow the cammera sensor to warmup,
 # and initialize the FPS counter
 print("[INFO] starting video stream...")
@@ -42,35 +78,41 @@ fps = FPS().start()
 
 # loop over the frames from the video stream
 while True:
-	# grab the frame from the threaded video stream and resize it
-	# to have a maximum width of 400 pixels
+	# grab the frame from the threaded video stream, resize it, and
+	# grab its imensions
 	frame = vs.read()
 	frame = imutils.resize(frame, width=400)
+	(fH, fW) = frame.shape[:2]
 
-	# grab the frame dimensions and convert it to a blob
-	(h, w) = frame.shape[:2]
-	blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)),
-		0.007843, (300, 300), 127.5)
+	# if the input queue *is* empty, give the current frame to
+	# classify
+	if inputQueue.empty():
+		inputQueue.put(frame)
 
-	# pass the blob through the network and obtain the detections and
-	# predictions
-	net.setInput(blob)
-	detections = net.forward()
+	# if the output queue *is not* empty, grab the detections
+	if not outputQueue.empty():
+		detections = outputQueue.get()
 
-	# loop over the detections
-	for i in np.arange(0, detections.shape[2]):
-		# extract the confidence (i.e., probability) associated with
-		# the prediction
-		confidence = detections[0, 0, i, 2]
+	# check to see if our detectios are not None (and if so, we'll
+	# draw the detections on the frame)
+	if detections is not None:
+		# loop over the detections
+		for i in np.arange(0, detections.shape[2]):
+			# extract the confidence (i.e., probability) associated
+			# with the prediction
+			confidence = detections[0, 0, i, 2]
 
-		# filter out weak detections by ensuring the `confidence` is
-		# greater than the minimum confidence
-		if confidence > args["confidence"]:
-			# extract the index of the class label from the
-			# `detections`, then compute the (x, y)-coordinates of
-			# the bounding box for the object
+			# filter out weak detections by ensuring the `confidence`
+			# is greater than the minimum confidence
+			if confidence < args["confidence"]:
+				continue
+
+			# otherwise, extract the index of the class label from
+			# the `detections`, then compute the (x, y)-coordinates
+			# of the bounding box for the object
 			idx = int(detections[0, 0, i, 1])
-			box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+			dims = np.array([fW, fH, fW, fH])
+			box = detections[0, 0, i, 3:7] * dims
 			(startX, startY, endX, endY) = box.astype("int")
 
 			# draw the prediction on the frame
